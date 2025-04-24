f97e1dca64446e6954a24898785f54170e51a6dd
D3fus <D3fus@users.noreply.github.com>
2025-04-24 10:05:37 +0000

    ✅ Ready to clone and code.

--- testing/ext/commands.py
+++ testing/ext/commands.py
@@ -1,8 +1,8 @@
 import click
 
-from project_name.ext.auth import create_user
-from project_name.ext.database import db
-from project_name.models import Product
+from testing.ext.auth import create_user
+from testing.ext.database import db
+from testing.models import Product
 
 
 def create_db():
